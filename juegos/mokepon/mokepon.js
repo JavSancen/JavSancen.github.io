0419a35b9d092efe998ef8b96849650d54512fad
JavSancen <fr.sancen@gmail.com>
2022-08-08 21:26:55 -0500

    adding DOM manipulation

--- juegos/mokepon/mokepon.js
+++ juegos/mokepon/mokepon.js
@@ -1,10 +1,23 @@
 function startGame() {
-    let botonPetPlayer = document.getElementById("boton-pet")
-    botonPetPlayer.addEventListener("click", selectPetPlayer)
+    let botonPetPlayer = document.getElementById('boton-pet')
+    botonPetPlayer.addEventListener('click', selectPetPlayer)
 }
 
 function selectPetPlayer() {
-    alert("SELECCONASTE TU MASCOTA")
+    let inputHipodoge = document.getElementById('Hipodoge')
+    let inputCapipepo = document.getElementById('Capipepo')
+    let inputRatigueya = document.getElementById('Ratigueya')
+    let spanPetPlayer = document.getElementById('pet-player')
+
+    if (inputHipodoge.checked) {
+        spanPetPlayer.innerHTML = 'Hipodoge'
+    } else if (inputCapipepo.checked) {
+        spanPetPlayer.innerHTML = 'Capipepo'
+    } else if (inputRatigueya.checked) {
+        spanPetPlayer.innerHTML = 'Ratigueya'
+    } else {
+        alert('Selecciona a una mascota')
+    }
 }
 
-window.addEventListener.apply("load", startGame)
+window.addEventListener('load', startGame)
