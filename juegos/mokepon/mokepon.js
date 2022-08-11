71716d76a5e64a9988fa868c822f85c9fd58dd24
JavSancen <fr.sancen@gmail.com>
2022-08-10 20:12:54 -0500

    adding atacks to my pets and random function to select enemy atack

--- juegos/mokepon/mokepon.js
+++ juegos/mokepon/mokepon.js
@@ -1,6 +1,16 @@
+let ataquePlayer
+let ataqueEnemy
+
 function startGame() {
     let botonPetPlayer = document.getElementById('boton-pet')
     botonPetPlayer.addEventListener('click', selectPetPlayer)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function selectPetPlayer() {
@@ -18,6 +28,49 @@ function selectPetPlayer() {
     } else {
         alert('Selecciona a una mascota')
     }
+    selectPetEnemy()
+}
+
+function selectPetEnemy() {
+    let petAleatorio = aleatorio(1,3)
+    let spanPetEnemy = document.getElementById('pet-enemy')
+
+    if (petAleatorio == 1) {
+        spanPetEnemy.innerHTML = 'Hipodoge'
+    } else if (petAleatorio == 2) {
+        spanPetEnemy.innerHTML = 'Capipepo'
+    } else {
+        spanPetEnemy.innerHTML = 'Ratigueya'
+    }
+}
+
+function ataqueFuego() {
+    ataquePlayer = 'FUEGO'
+    ataqueAleatorioEnemy()
+}
+function ataqueAgua() {
+    ataquePlayer = 'AGUA'
+    ataqueAleatorioEnemy()
+}
+function ataqueTierra() {
+    ataquePlayer = 'TIERRA'
+    ataqueAleatorioEnemy()
+}
+function ataqueAleatorioEnemy() {
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio == 1) {
+        ataqueEnemy = 'FUEGO'
+    } else if (ataqueAleatorio == 2) {
+        ataqueEnemy = 'AGUA'
+    } else {
+        ataqueEnemy = 'TIERRA'
+    }
+}
+
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener('load', startGame)
